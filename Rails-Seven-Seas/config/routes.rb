63b06f947a5c0107437e0da6369a9e0e6541b753
callas4ever <thetseringnorbu@gmail.com>
2020-06-05 13:02:30 -0400

    push from Tsering

--- Rails-Seven-Seas/config/routes.rb
+++ Rails-Seven-Seas/config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-
+  # get '/', to: "seas#welcome"
+  resources :seas, only: [:index, :new, :edit, :show, :create, :update]
+ 
+  # get '/', to: 'seas#welcome'
+  # get '/seas/:id', to: 'seas#show'
+  # get '/seas/new', to: 'seas#create'
+  
 end
